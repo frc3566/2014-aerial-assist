c03d599019a14f47dedf5cd03b3780005a9961a6
Seth Battis <SethBattis@stmarksschool.org>
2014-02-16 09:06:03 -0500

    Added EjectBall to RobotBuilder
    What subsystem handles this? Is this the flipper?

--- OfficialCode/src/org/usfirst/frc3566/OfficialCode/commands/EjectBall.java
+++ OfficialCode/src/org/usfirst/frc3566/OfficialCode/commands/EjectBall.java
@@ -3,9 +3,7 @@
  * and open the template in the editor.
  */
 package org.usfirst.frc3566.OfficialCode.commands;
-
 import edu.wpi.first.wpilibj.command.Command;
-
 /**
  *
  * @author robotics
@@ -16,24 +14,19 @@ public class EjectBall extends Command {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
-
     // Called just before this Command runs the first time
     protected void initialize() {
     }
-
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     }
-
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
-
     // Called once after isFinished returns true
     protected void end() {
     }
-
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
