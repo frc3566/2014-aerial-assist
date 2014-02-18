3ea56c4a4946f64128395dcad7785342358a0249
KaiwenWu <KaiwenWu@stmarksschool.org>
2014-02-17 20:16:27 -0500

    Numbers Adjustments

--- OfficialCode/src/org/usfirst/frc3566/OfficialCode/RobotConstants.java
+++ OfficialCode/src/org/usfirst/frc3566/OfficialCode/RobotConstants.java
@@ -25,7 +25,7 @@ public class RobotConstants {
     public static final double AUTONOMOUS_SPEED_TO_DRIVE_AT_HOT_GOAL = 1.0; // percentage of full speed
     public static final double AUTONOMOUS_SPEED_TO_DRIVE_AT_COLD_GOAL = 1.0; // percentage of full speed
     public static final double AUTONOMOUS_TIME_TO_WAIT_FOR_COLD_GOAL = 2.0; // seconds
-    public static final double AUTONOMOUS_SPEED_TO_LOWER_EL_TORO = 0.35; // percentage of full speed
+    public static final double AUTONOMOUS_SPEED_TO_LOWER_EL_TORO = -0.35; // percentage of full speed
     
     public static final double EL_TORO_RAISE_SPEED = -1.0;
     public static final double EL_TORO_LOWER_SPEED = 1.0;
@@ -34,8 +34,8 @@ public class RobotConstants {
     public static final double EL_TORO_TIME_TO_VOMIT_BALL = 2.5; // seconds
     
     public static final double WINCH_WIND_SPEED = 1.0; // percentage of full speed, FIXME WAG
-    public static final double DOGBOX_ENGAGE_SPEED = -0.4; // percentage of full speed, FIXME WAG
-    public static final double DOGBOX_DISENGAGE_SPEED = 1.0; // percentage of full speed, FIXME WAG
+    public static final double DOGBOX_ENGAGE_SPEED = 1.0; // percentage of full speed, FIXME WAG
+    public static final double DOGBOX_DISENGAGE_SPEED = -1.0; // percentage of full speed, FIXME WAG
     public static final double DOGBOX_DISENAGE_TIME = 60.0/*seconds*/ / 72.0/*rpm*/ / 12.0/*fraction of turn*/ * (1.0 / Math.abs(DOGBOX_DISENGAGE_SPEED)); // turn 1/2 totation
     
     public static final double DRIVETRAIN_DISTANCE_TO_FULL_SPEED = 5.0; // feet, FIXME WAG
